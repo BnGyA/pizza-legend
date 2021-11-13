d89d46455d1822825ad06b84a559c03429643cc0
BenjaminRochez <rochez.benjamin@gmail.com>
2021-11-13 08:20:08 +0100

    Added combatants & battle scene

--- Overworld.js
+++ Overworld.js
@@ -1,10 +1,10 @@
 class Overworld {
- constructor(config) {
-   this.element = config.element;
-   this.canvas = this.element.querySelector(".game-canvas");
-   this.ctx = this.canvas.getContext("2d");
-   this.map = null;
- }
+  constructor(config) {
+    this.element = config.element;
+    this.canvas = this.element.querySelector(".game-canvas");
+    this.ctx = this.canvas.getContext("2d");
+    this.map = null;
+  }
 
   startGameLoop() {
     const step = () => {
@@ -15,72 +15,72 @@ class Overworld {
       const cameraPerson = this.map.gameObjects.hero;
 
       //Update all objects
-      Object.values(this.map.gameObjects).forEach(object => {
+      Object.values(this.map.gameObjects).forEach((object) => {
         object.update({
           arrow: this.directionInput.direction,
           map: this.map,
-        })
-      })
+        });
+      });
 
       //Draw Lower layer
       this.map.drawLowerImage(this.ctx, cameraPerson);
 
       //Draw Game Objects
-      Object.values(this.map.gameObjects).sort((a,b) => {
-        return a.y - b.y;
-      }).forEach(object => {
-        object.sprite.draw(this.ctx, cameraPerson);
-      })
+      Object.values(this.map.gameObjects)
+        .sort((a, b) => {
+          return a.y - b.y;
+        })
+        .forEach((object) => {
+          object.sprite.draw(this.ctx, cameraPerson);
+        });
 
       //Draw Upper layer
       this.map.drawUpperImage(this.ctx, cameraPerson);
-      
+
       requestAnimationFrame(() => {
-        step();   
-      })
-    }
+        step();
+      });
+    };
     step();
- }
+  }
 
- bindActionInput() {
-   new KeyPressListener("Enter", () => {
-     //Is there a person here to talk to?
-     this.map.checkForActionCutscene()
-   })
- }
+  bindActionInput() {
+    new KeyPressListener("Enter", () => {
+      //Is there a person here to talk to?
+      this.map.checkForActionCutscene();
+    });
+  }
 
- bindHeroPositionCheck() {
-   document.addEventListener("PersonWalkingComplete", e => {
-     if (e.detail.whoId === "hero") {
-       //Hero's position has changed
-       this.map.checkForFootstepCutscene()
-     }
-   })
- }
+  bindHeroPositionCheck() {
+    document.addEventListener("PersonWalkingComplete", (e) => {
+      if (e.detail.whoId === "hero") {
+        //Hero's position has changed
+        this.map.checkForFootstepCutscene();
+      }
+    });
+  }
 
- startMap(mapConfig) {
-  this.map = new OverworldMap(mapConfig);
-  this.map.overworld = this;
-  this.map.mountObjects();
- }
+  startMap(mapConfig) {
+    this.map = new OverworldMap(mapConfig);
+    this.map.overworld = this;
+    this.map.mountObjects();
+  }
 
- init() {
-  this.startMap(window.OverworldMaps.DemoRoom);
+  init() {
+    this.startMap(window.OverworldMaps.DemoRoom);
 
+    this.bindActionInput();
+    this.bindHeroPositionCheck();
 
-  this.bindActionInput();
-  this.bindHeroPositionCheck();
+    this.directionInput = new DirectionInput();
+    this.directionInput.init();
 
-  this.directionInput = new DirectionInput();
-  this.directionInput.init();
+    this.startGameLoop();
 
-  this.startGameLoop();
-
-
-  // this.map.startCutscene([
-  //   { type: "changeMap", map: "DemoRoom"}
-  //   // { type: "textMessage", text: "This is the very first message!"}
-  // ])
-
- }
+    this.map.startCutscene([
+      { type: "battle" },
+      //{ type: "changeMap", map: "DemoRoom"}
+      // { type: "textMessage", text: "This is the very first message!"}
+    ]);
+  }
 }
--- OverworldEvent.js
+++ OverworldEvent.js
@@ -1,80 +1,92 @@
 class OverworldEvent {
-  constructor({ map, event}) {
+  constructor({ map, event }) {
     this.map = map;
     this.event = event;
   }
 
   stand(resolve) {
-    const who = this.map.gameObjects[ this.event.who ];
-    who.startBehavior({
-      map: this.map
-    }, {
-      type: "stand",
-      direction: this.event.direction,
-      time: this.event.time
-    })
-    
+    const who = this.map.gameObjects[this.event.who];
+    who.startBehavior(
+      {
+        map: this.map,
+      },
+      {
+        type: "stand",
+        direction: this.event.direction,
+        time: this.event.time,
+      }
+    );
+
     //Set up a handler to complete when correct person is done walking, then resolve the event
-    const completeHandler = e => {
+    const completeHandler = (e) => {
       if (e.detail.whoId === this.event.who) {
         document.removeEventListener("PersonStandComplete", completeHandler);
         resolve();
       }
-    }
-    document.addEventListener("PersonStandComplete", completeHandler)
+    };
+    document.addEventListener("PersonStandComplete", completeHandler);
   }
 
   walk(resolve) {
-    const who = this.map.gameObjects[ this.event.who ];
-    who.startBehavior({
-      map: this.map
-    }, {
-      type: "walk",
-      direction: this.event.direction,
-      retry: true
-    })
+    const who = this.map.gameObjects[this.event.who];
+    who.startBehavior(
+      {
+        map: this.map,
+      },
+      {
+        type: "walk",
+        direction: this.event.direction,
+        retry: true,
+      }
+    );
 
     //Set up a handler to complete when correct person is done walking, then resolve the event
-    const completeHandler = e => {
+    const completeHandler = (e) => {
       if (e.detail.whoId === this.event.who) {
         document.removeEventListener("PersonWalkingComplete", completeHandler);
         resolve();
       }
-    }
-    document.addEventListener("PersonWalkingComplete", completeHandler)
-
+    };
+    document.addEventListener("PersonWalkingComplete", completeHandler);
   }
 
   textMessage(resolve) {
-
     if (this.event.faceHero) {
       const obj = this.map.gameObjects[this.event.faceHero];
-      obj.direction = utils.oppositeDirection(this.map.gameObjects["hero"].direction);
+      obj.direction = utils.oppositeDirection(
+        this.map.gameObjects["hero"].direction
+      );
     }
 
     const message = new TextMessage({
       text: this.event.text,
-      onComplete: () => resolve()
-    })
-    message.init( document.querySelector(".game-container") )
+      onComplete: () => resolve(),
+    });
+    message.init(document.querySelector(".game-container"));
   }
 
   changeMap(resolve) {
-
     const sceneTransition = new SceneTransition();
     sceneTransition.init(document.querySelector(".game-container"), () => {
-      this.map.overworld.startMap( window.OverworldMaps[this.event.map] );
+      this.map.overworld.startMap(window.OverworldMaps[this.event.map]);
       resolve();
 
       sceneTransition.fadeOut();
+    });
+  }
 
-    })
+  battle(resolve) {
+    const battle = new Battle({
+      onComplete: () => {
+        resolve();
+      },
+    });
+    battle.init(document.querySelector(".game-container"));
   }
 
   init() {
-    return new Promise(resolve => {
-      this[this.event.type](resolve)      
-    })
+    return new Promise((resolve) => {
+      this[this.event.type](resolve);
+    });
   }
-
 }
